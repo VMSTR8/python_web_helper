adf7b86df0bc8274117f6a5d1506daea08c6942c
Maxim Vinokurov <vmstr8@yandex.ru>
2021-02-08 21:38:40 +0300

    Оставил только те маршруты, над которыми ведется работа.

--- market_search/urls.py
+++ market_search/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
-from django.conf.urls import url
 
-from .views import *
-from market_search import views
+from market_search.views import *
 
 urlpatterns = [
     path('search/', search),
     path('search/result/', SearchResultView.as_view(), name='search_result'),
 
-    url(r'^dbwrite/$', views.dbwrite),
+    path('dbwrite/', write_to_db)
 ]
