d48f33bdc33658eabb65ff37133875f66774773c
Maxim Vinokurov <vmstr8@yandex.ru>
2021-02-10 01:18:08 +0300

    Модели расставлены в правильном порядке. Убран атрибут unique у поля item_name модели Items. Для link установлен URLField.

--- market_search/models.py
+++ market_search/models.py
@@ -3,27 +3,6 @@ from django.contrib.postgres.search import SearchVectorField
 from django.contrib.postgres.indexes import GinIndex
 
 
-# Model of market items
-class Items(models.Model):
-    item_name = models.CharField(max_length=255, unique=True, verbose_name='Наименование товара')
-    in_stock = models.BooleanField(default=False, verbose_name='Наличие')
-    link = models.TextField(verbose_name='Ссылка')
-    price = models.IntegerField(null=True, blank=True, verbose_name='Цена магазина')
-    store_name = models.CharField(max_length=30, verbose_name='Наименование магазина')
-    category = models.ForeignKey('Categories', on_delete=models.PROTECT, null=True, blank=True,
-                                 verbose_name='Категория')
-    store = models.ForeignKey('Stores', on_delete=models.PROTECT, null=True, blank=True, verbose_name='Магазин')
-    search_vector = SearchVectorField(null=True, blank=True)
-
-    def __str__(self):
-        return self.item_name
-
-    class Meta(object):
-        verbose_name = 'Товар'
-        verbose_name_plural = 'Товары'
-        indexes = [GinIndex(fields=['search_vector'])]
-
-
 # Categories for items
 class Categories(models.Model):
     title = models.CharField(max_length=50, db_index=True, verbose_name='Категория')
@@ -49,3 +28,27 @@ class Stores(models.Model):
     class Meta:
         verbose_name = 'Магазин'
         verbose_name_plural = 'Магазины'
+
+
+# Model of market items
+class Items(models.Model):
+    item_pic = models.URLField(verbose_name='Картинка', null=True)
+    item_name = models.CharField(max_length=255, verbose_name='Наименование товара')
+    in_stock = models.BooleanField(default=False, verbose_name='Наличие')
+    link = models.URLField(verbose_name='Ссылка')
+    price = models.IntegerField(null=True, blank=True, verbose_name='Цена магазина')
+    store_name = models.CharField(max_length=30, verbose_name='Наименование магазина')
+    category = models.ForeignKey('Categories', on_delete=models.PROTECT, null=True, blank=True,
+                                 verbose_name='Категория')
+    store = models.ForeignKey('Stores', on_delete=models.PROTECT, null=True, blank=True, verbose_name='Магазин')
+    search_vector = SearchVectorField(null=True, blank=True)
+
+    owner = models.ForeignKey('auth.User', related_name='items', on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return self.item_name
+
+    class Meta(object):
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        indexes = [GinIndex(fields=['search_vector'])]
